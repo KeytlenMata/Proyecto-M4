2eb46ce22439e334d0552ef5d5260b0c1de845fc
Keytlen <keytlen.mata@uip.edu.pa>
2025-10-25 03:29:03 -0500

    Agregar historia.py con datos básicos del protagonista

---
+++ historia.py
@@ -0,0 +1,6 @@
+# Parte 1: Información básica del protagonista 
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+ciudad = input("Ingresa tu ciudad de residencia: ")
+
+print(f"¡Hola, {nombre}! Tienes {edad} años y vives en {ciudad}.")
