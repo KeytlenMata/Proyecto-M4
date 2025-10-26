eda3590852ade7e9a1555045b3894a6de6b9b7ef
GeoHannaUIP <georgina.hanna@uip.edu.pa>
2025-10-25 19:28:52 -0500

    Parte 2. Continuacion de la historia

--- historia.py
+++ historia.py
@@ -8,3 +8,17 @@ print(f"¡Hola, {nombre}! Tienes {edad} años y vives en {ciudad}.")
 print("\nNunca pensaste que tu vida cambiaría por un mensaje de texto anónimo...")
 print("Solo decía: 'No confíes en nadie. Ni siquiera en ti.'")
 
+# Parte 2: continua la historia
+
+print("\nTu corazón late más rápido. No reconoces el número que envió el mensaje.")
+print("Sientes una mezcla de curiosidad y miedo.")
+
+print("\nEl silencio se vuelve pesado... el teléfono vibra otra vez.")
+print(f"Nuevo mensaje: 'Ya te vi {nombre}. No mires por la ventana.'")
+
+
+
+
+
+
+
